1191ace3a87faa4b9138871ac4cbc051e8b216fe
Oscar Gomez <odgomezlopez@gmail.com>
2024-09-02 00:52:26 +0200

    Guardado en Memoria local

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -1,19 +1,26 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const MyContext = createContext('Default value');
 
 export function MyProvider({ children }) {
     //Se define un estado y un set para cada uno de los datos que se quiera almacenar
-    const [user, setUser] = useState('Oscar Gomez');
-  
+    const [user, setUser] = useState(localStorage.getItem('userName') ?? 'Oscar Gomez');
+
     const updateUser = (newUser) => {
-      setUser(newUser);
+        setUser(newUser);
+        //localStorage.setItem('userName', user);
     };
-    
+
+    // useEffect para guardar el usuario en localStorage cuando cambie
+    useEffect(() => {
+        localStorage.setItem('userName', user);
+    }, [user]); // Se ejecuta cada vez que 'user' cambie
+
+
     //Render utilizando el contexto general y los hijos del componente MyProvider
     return (
-      <MyContext.Provider value={{ user, updateUser }}>
-        {children}
-      </MyContext.Provider>
+        <MyContext.Provider value={{ user, updateUser }}>
+            {children}
+        </MyContext.Provider>
     );
-  }
+}
